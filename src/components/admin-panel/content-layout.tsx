bd00543d42cfa9782e5d58cb264bde9cf56eef24
hassan <hassan.jer78@gmail.com>
2024-12-10 15:36:21 +0100

    save work

--- src/components/admin-panel/content-layout.tsx
+++ src/components/admin-panel/content-layout.tsx
@@ -7,13 +7,11 @@ interface ContentLayoutProps {
 
 export function ContentLayout({ title, children }: ContentLayoutProps) {
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
+    <div className="flex flex-col h-full">
       <Navbar title={title} />
-      <main className="flex-1 min-h-0">
-        <div className="h-full pt-8 pb-8 px-4 sm:px-8 overflow-y-auto">
-          {children}
-        </div>
-      </main>
+      <div className="h-full pt-8 pb-8 px-4 sm:px-8 overflow-auto">
+        {children}
+      </div>
     </div>
   );
 }
