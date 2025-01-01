e7770c8b842271f16a92e02db0066dd6b3fb7cec
hassan <hassan.jer78@gmail.com>
2025-01-01 15:37:09 +0100

    create page of products list and hook

--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -1,8 +1,97 @@
-import React from 'react'
+"use client"
 
+import React from 'react'
+import { useProducts } from '@/hooks/useProduct'
+import { useFormat } from '@/hooks/useFormat'
+
+import {  ShoppingCart } from 'lucide-react'
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  import { Card } from '@/components/ui/card'
+
+import { Loader2 } from 'lucide-react'
+
+import { IProduct } from '@/types/Product.interface'
 
 export default function ProductsPage() {
+  const { data, isLoading } = useProducts()
+  const { formatCurrency, formatPercentage } = useFormat()
+
+  if(isLoading) return <div className='flex justify-center items-center h-screen'><Loader2 className="w-6 h-6 animate-spin" /></div>
+
   return (
-    <div>Products</div>
+    <div className="p-6">
+        <h1 className="text-2xl font-bold font-dmSans flex items-center">
+            <ShoppingCart className="w-6 h-6 mr-2" />
+            Produits
+        </h1>
+
+        <Card className="flex flex-col mt-6">
+           <Table >
+            <TableHeader>
+                <TableRow>
+                    
+                    <TableHead>
+                        <p className='text-sm font-medium text-nowrap'>
+                            Nom
+                        </p>
+                    </TableHead>
+                    <TableHead className='w-1/2'>
+                        <p className='text-sm font-medium text-nowrap'>
+                            Description
+                        </p>
+                    </TableHead>
+                    <TableHead>
+                      <p className='text-sm font-medium text-nowrap'>
+                        Taux de TVA
+                      </p>
+                    </TableHead>
+                    <TableHead>
+                        <p className='text-sm font-medium text-nowrap'>
+                            Prix
+                        </p>
+                    </TableHead>
+                   
+                    
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {data?.data.map((product: IProduct) => (
+                    <TableRow key={product.product_id} className="hover:bg-gray-100 cursor-pointer">
+
+                        <TableCell>
+                            <p className='text-sm font-medium text-nowrap'>
+                            {product.name}
+                            </p>
+                        </TableCell>
+
+                        <TableCell>
+                            <p className='text-sm font-medium text-ellipsis overflow-hidden line-clamp-3'>
+                            {product.description}
+                            </p>
+                        </TableCell>
+                        <TableCell>
+                            <p className='text-sm font-medium text-nowrap'>
+                            {formatPercentage(product.vat_rate)}
+                            </p>
+                        </TableCell>
+                        <TableCell>
+                            <p className='text-sm font-medium text-nowrap'>
+                            {formatCurrency(product.price_excluding_tax)}
+                            </p>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+           </Table>
+        </Card>
+    </div>
   )
 }
---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,60 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { api } from "@/services/api"
+import type { ICreateProductRequest } from "@/types/Product.request.interface"
+import type { IUpdateProductRequest } from "@/types/Product.request.interface"
+
+
+
+export const useProducts = () => {
+    return useQuery({
+        queryKey: ["products"],
+        queryFn: () => api.get("/products"),
+        
+        staleTime: 1000 * 60 * 5, // 5 minutes
+    })
+}
+
+export const useProduct = (productId: number) => {
+    return useQuery({
+        queryKey: ["products", productId],
+        queryFn: () => api.get(`/products/${productId}`),
+        enabled: !!productId,
+    })
+}
+
+export const useCreateProduct = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: (data: ICreateProductRequest) => 
+            api.post("/products", data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["products"] })
+        },
+    })
+}
+
+export const useUpdateProduct = (productId: number) => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: (data: IUpdateProductRequest) =>
+            api.put(`/products/${productId}`, data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["products", productId] })
+            queryClient.invalidateQueries({ queryKey: ["products"] })
+        },
+    })
+}
+
+export const useDeleteProduct = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: (productId: number) =>
+            api.delete(`/products/${productId}`),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["products"] })
+        },
+    })
+}
