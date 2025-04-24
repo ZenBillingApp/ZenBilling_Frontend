87924b92cb5a6f043d4585e31f007a13b0b215ef
Hassan <hassan@onlyonecard.eu>
2025-04-24 21:07:36 +0200

    Refactorise le formulaire d'inscription en intégrant des composants FormField, FormItem et FormControl pour une meilleure gestion des formulaires. Améliore la structure du code et la validation des champs, tout en simplifiant la soumission du formulaire.

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -9,11 +9,11 @@ import { IRegisterRequest } from "@/types/Auth.interface";
 
 import { authClient, getErrorMessageFR } from "@/lib/auth-client";
 
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { redirect } from "next/navigation";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 
 import { Loader2 } from "lucide-react";
 
@@ -31,7 +31,8 @@ const registerSchema = z.object({
 export function RegisterForm() {
     const { toast } = useToast();
     const [isLoading, setIsLoading] = useState(false);
-    const { register, handleSubmit, formState: { errors } } = useForm<IRegisterRequest>({
+    
+    const form = useForm<IRegisterRequest>({
         resolver: zodResolver(registerSchema),
     });
 
@@ -70,72 +71,94 @@ export function RegisterForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex flex-col gap-6">
-            <div className="flex flex-col items-center gap-2">
-            <a
-              href="#"
-              className="flex flex-col items-center gap-2 font-medium"
-            >
-              <div className="flex h-8 w-8 items-center justify-center rounded-md">
-                <Image src={logo} alt="zenbilling logo" width={32} height={32} />
-              </div>
-              <span className="sr-only">
-                ZenBilling  
-              </span>
-            </a>
-            <h1 className="text-xl font-bold">
-                Créer un compte
-            </h1>
-            <div className="text-center text-sm">
-              Vous avez déjà un compte?{" "}
-              <a href="/login" className="underline underline-offset-4">
-                Connexion
-              </a>
-            </div>
-          </div>
-          {/* {handleRegister.error && handleRegister.error.response?.data.errors && (
-            <Alert variant="destructive">
-              <AlertCircle className="h-4 w-4" />
-              <AlertTitle>Une erreur est survenue lors de la création du compte</AlertTitle>
-              <AlertDescription>
-                <ul className="list-disc pl-5">
-                  {handleRegister.error.response?.data.errors.map((error) => (
-                    <li key={error.field}>{error.message}</li>
-                  ))}
-                </ul>
-              </AlertDescription>
-            </Alert>
-          )} */}
-                <div className="flex gap-2">
-                    <div className="flex flex-col w-1/2 items-start gap-2">
-                        <Label htmlFor="first_name">Prénom</Label>
-                        <Input type="text" id="first_name" {...register("first_name")} />
-                    {errors.first_name && <p className="text-red-500 text-xs italic">{errors.first_name.message}</p>}
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6">
+                <div className="flex flex-col items-center gap-2">
+                    <a
+                        href="#"
+                        className="flex flex-col items-center gap-2 font-medium"
+                    >
+                        <div className="flex h-8 w-8 items-center justify-center rounded-md">
+                            <Image src={logo} alt="zenbilling logo" width={32} height={32} />
+                        </div>
+                        <span className="sr-only">
+                            ZenBilling  
+                        </span>
+                    </a>
+                    <h1 className="text-xl font-bold">
+                        Créer un compte
+                    </h1>
+                    <div className="text-center text-sm">
+                        Vous avez déjà un compte?{" "}
+                        <a href="/login" className="underline underline-offset-4">
+                            Connexion
+                        </a>
                     </div>
-                    <div className="flex flex-col w-1/2 items-start gap-2">
-                        <Label htmlFor="last_name">Nom</Label>
-                        <Input type="text" id="last_name" {...register("last_name")} />
-                        {errors.last_name && <p className="text-red-500 text-xs italic">{errors.last_name.message}</p>}
                 </div>
-                </div>
-                <div className="flex flex-col items-start gap-2">
-                    <Label htmlFor="email">Email</Label>
-                    <Input type="email" id="email" {...register("email")} />
-                    {errors.email && <p className="text-red-500 text-xs italic">{errors.email.message}</p>}
-                </div>
-                <div className="flex flex-col items-start gap-2">
-                    <Label htmlFor="password">Mot de passe</Label>
-                    <Input type="password" id="password" {...register("password")} />
-                    {errors.password && <p className="text-red-500 text-xs italic">{errors.password.message}</p>}
-                </div>
-               
-                <Button type="submit" disabled={isLoading}>
-                    {isLoading ? <Loader2 className="w-4 h-4 animate-spin" /> : "S&apos;inscrire"}
-                </Button>
-            </div>
-        </form>
-    )
 
+                <div className="flex gap-2">
+                    <FormField
+                        control={form.control}
+                        name="first_name"
+                        render={({ field }) => (
+                            <FormItem className="w-1/2">
+                                <FormLabel>Prénom</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    
+                    <FormField
+                        control={form.control}
+                        name="last_name"
+                        render={({ field }) => (
+                            <FormItem className="w-1/2">
+                                <FormLabel>Nom</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Mot de passe</FormLabel>
+                            <FormControl>
+                                <Input type="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                
+                <Button type="submit" disabled={isLoading}>
+                    {isLoading ? <Loader2 className="w-4 h-4 animate-spin" /> : "S'inscrire"}
+                </Button>
+            </form>
+        </Form>
+    )
 }
 
