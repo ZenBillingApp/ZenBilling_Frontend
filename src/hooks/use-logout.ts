d4b7318d5eec285ba40c2d87f67415d05e8bdeb8
Hassan <hassan@onlyonecard.eu>
2025-04-24 13:30:41 +0200

    Ajoute la fonction de nettoyage du cache des requêtes lors de la déconnexion, améliorant ainsi la gestion de l'état de l'application après la déconnexion de l'utilisateur.

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -3,16 +3,17 @@
 import { useRouter } from "next/navigation"
 import { authClient } from "@/lib/auth-client"
 import { useAuthStore } from "@/stores/authStores"
-
+import { useQueryClient } from "@tanstack/react-query"
 export function useLogout() {
   const router = useRouter()
   const clearAuth = useAuthStore.getState().clearAuth
-
+  const queryClient = useQueryClient()
   const logout = async (redirectTo: string = "/login") => {
     await authClient.signOut({
       fetchOptions: {
         onSuccess: async () => {
           await clearAuth()
+          queryClient.clear()
           router.push(redirectTo)
         }
       }
